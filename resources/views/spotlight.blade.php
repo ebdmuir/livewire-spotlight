fd05080b7b10ee4304d63ae32dc80c8365a7debf
Eric Muir <eric@ebdm.dev>
2023-02-05 16:42:10 +0000

    Add white default with tailwind dark mode

--- resources/views/spotlight.blade.php
+++ resources/views/spotlight.blade.php
@@ -26,17 +26,17 @@
              x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-150"
              x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
              class="fixed inset-0 transition-opacity">
-            <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
+            <div class="absolute inset-0 bg-white dark:bg-gray-900 opacity-50"></div>
         </div>
 
         <div x-show="isOpen" x-transition:enter="ease-out duration-200"
              x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
              x-transition:leave="ease-in duration-150" x-transition:leave-start="opacity-100 scale-100"
              x-transition:leave-end="opacity-0 scale-95"
-             class="relative bg-gray-900 rounded-lg overflow-hidden shadow-xl transform transition-all max-w-lg w-full">
+             class="relative bg-white dark:bg-gray-900 border border-gray-800 rounded-lg overflow-hidden shadow-xl transform transition-all max-w-lg w-full">
             <div class="relative">
                 <div class="absolute h-full right-5 flex items-center">
-                    <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    <svg class="animate-spin h-5 w-5 dark:text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
                          viewBox="0 0 24 24" wire:loading.delay>
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
                                 stroke-width="4"></circle>
@@ -48,7 +48,7 @@
                        @keydown.prevent.arrow-down="selectDown()" x-ref="input" x-model="input"
                        type="text"
                        style="caret-color: #6b7280; border: 0 !important;"
-                       class="appearance-none w-full bg-transparent px-6 py-4 text-gray-300 text-lg placeholder-gray-500 focus:border-0 focus:border-transparent focus:shadow-none outline-none focus:outline-none"
+                       class="appearance-none w-full bg-transparent px-6 py-4 text-gray-700 dark:text-gray-300 text-lg placeholder-gray-500 focus:border-0 focus:border-transparent focus:shadow-none outline-none focus:outline-none"
                        x-bind:placeholder="inputPlaceholder">
             </div>
             <div class="border-t border-gray-800" x-show="filteredItems().length > 0" style="display: none;">
@@ -58,7 +58,7 @@
                             <button @click="go(item[0].item.id)" class="block w-full px-6 py-3 text-left"
                                     :class="{ 'bg-gray-700': selected === i, 'hover:bg-gray-800': selected !== i }">
                                 <span x-text="item[0].item.name"
-                                      :class="{'text-gray-300': selected !== i, 'text-white': selected === i }"></span>
+                                      :class="{'text-gray-700 dark:text-gray-300': selected !== i, 'text-white': selected === i }"></span>
                                 <span x-text="item[0].item.description" class="ml-1"
                                       :class="{'text-gray-500': selected !== i, 'text-gray-400': selected === i }"></span>
                             </button>
